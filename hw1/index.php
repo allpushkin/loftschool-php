4560541eea9c2ff640e431e05357e47b565685b9
campside <chirkov.ma@mail.ru>
2017-07-09 13:06:01 +0300

    Code sniffer fixes

--- hw1/index.php
+++ hw1/index.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width,
+          user-scalable=no,
+          initial-scale=1.0,
+          maximum-scale=1.0,
+          minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossorigin="anonymous">
     <style>
         body {
             padding: 10px;
@@ -28,11 +34,11 @@
 
 <?php
 echo "<h2>Задание #1</h2>";
-$name = "Бессмертный Джа";
-$age = 190;
-echo "Меня зовут <b>$name</b>";
+$myName = "Бессмертный Джа";
+$myAge = 190;
+echo "Меня зовут <b>$myName</b>";
 echo "<br>";
-echo "Мне $age лет";
+echo "Мне $myAge лет";
 echo "<br>";
 echo "\"!|/\\'\"/";
 
@@ -41,18 +47,22 @@ $total = 80;
 $byPencil = 40;
 $byMarker = 23;
 $byPaint = $total - $byMarker - $byPencil;
-echo "На школьной выставке $total рисунков. Из них $byPencil выполнены карандашами, $byMarker фломастерами, 
-          остальные красками. Сколько рисунков выполнены красками?";
+echo "На школьной выставке $total рисунков. 
+        Из них $byPencil выполнены карандашами, $byMarker фломастерами, 
+        остальные красками. Сколько рисунков выполнены красками?";
 echo "<br>";
-echo "Количество рисунков выполненных красками = $total - $byMarker - $byPencil = $byPaint";
+echo "Количество рисунков выполненных красками = 
+        $total - $byMarker - $byPencil = $byPaint";
 
 echo "<h2>Задание #3</h2>";
 define("CONST", 777, true);
-if (defined("CONST") == true) echo "Константа CONST объявлена и равна " . constant("CONST");
+if (defined("CONST") == true)
+    echo "Константа CONST объявлена и равна " . constant("CONST");
 
 echo "<h2>Задание #4</h2>";
 echo "Сколько вам лет? ";
-echo "<input class=\"input-group mb-2 mr-sm-2 mb-sm-0\" id=\"ageInput\" type=\"text\" onkeyup=\"task4(this.value)\">";
+echo "<input class=\"input-group mb-2 mr-sm-2 mb-sm-0\" 
+        id=\"ageInput\" type=\"text\" onkeyup=\"task4(this.value)\">";
 echo "<span class=\"h5\" id='result'>...</span>";
 
 echo "<h2>Задание #5</h2>";
@@ -60,27 +70,27 @@ echo "<h2>Задание #5</h2>";
 $day = rand(1, 7);
 
 switch ($day) {
-    case 1:
+case 1:
         $dayName = "Понедельник";
-        break;
-    case 2:
+    break;
+case 2:
         $dayName = "Вторник";
-        break;
-    case 3:
+    break;
+case 3:
         $dayName = "Среда";
-        break;
-    case 4:
+    break;
+case 4:
         $dayName = "Четверг";
-        break;
-    case 5:
+    break;
+case 5:
         $dayName = "Пятница";
-        break;
-    case 6:
+    break;
+case 6:
         $dayName = "Cуббота";
-        break;
-    case 7:
+    break;
+case 7:
         $dayName = "Воскресенье";
-        break;
+    break;
 }
 
 $dayType = ($day > 0 && $day < 6) ? "рабочий" : "выходной";
@@ -117,7 +127,8 @@ $cars = [$bmw, $toyota, $volvo];
 foreach ($cars as $item) {
     $carName = $item['name'];
     echo "CAR: <b>$carName</b>" . "<br>";
-    echo $item['model'] . " " . $item['speed'] . " " . $item['doors'] . " " . $item['year'] . "<br>";
+    echo $item['model'] . " " . $item['speed'] . " " .
+         $item['doors'] . " " . $item['year'] . "<br>";
 }
 
 echo "<h2>Задание #7</h2>";
