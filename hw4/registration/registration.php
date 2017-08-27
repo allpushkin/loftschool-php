be8749c52045054caa9bd25048db5cef38d5df94
campside <chirkovv.ma@yandex.ru>
2017-08-27 07:49:55 +0300

    Issue #10 Unnecessary line deleted

--- hw4/registration/registration.php
+++ hw4/registration/registration.php
@@ -1,19 +1,5 @@
 <?php
 $config = include 'db/config.php';
-
-$mysqli = new mysqli(
-    $config->host,
-    $config->username,
-    $config->password,
-    $config->database,
-    $config->port
-);
-
-$mysqli->set_charset('utf8');
-
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
 ?>
 
     <!DOCTYPE html>
